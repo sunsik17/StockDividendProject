601fef1d18444455af00c4b5d88dc1309d8ecbc0
sunsik <sunsig21c@gmail.com>
2023-03-12 18:51:59 +0900

    project init

---
+++ src/main/java/com/zerobase/stockdividendproject/StockDividendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.stockdividendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDividendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockDividendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/stockdividendproject/StockDividendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.stockdividendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDividendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
